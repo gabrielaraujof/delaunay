30dad5c86ec368c9e738c32c59e30cf783f41f99
Desi Cochrane <desi@desicochrane.com>
2018-10-07 10:07:41 +0200

    Add comments

--- src/PointSort.js
+++ src/PointSort.js
@@ -1,8 +1,10 @@
 export default function (A, B) {
+    // if they share same x-coord
     if (A[0] === B[0]) {
-        if (A[1] === B[1]) return 0
+        // then A comes before B if its y-coord is higher
         return A[1] > B[1] ? -1 : 1
     }
 
+    // otherwise A comes before B if its x-coord is lower
     return A[0] < B[0] ? -1 : 1
 }
