590b1bab733fdf174faa5386bfe440251b57117c
Desi Cochrane <desi@desicochrane.com>
2018-10-07 09:54:27 +0200

    simpler test case

--- __tests__/DelaunayTest.js
+++ __tests__/DelaunayTest.js
@@ -7,21 +7,22 @@ describe('Delaunay', () => {
     })
 
     it('Triangulates n points', () => {
-        //   D   F    J
-        //   C     G
-        // A   E      I
-        //   B        H
+        //   D     F   I
+        //   C       G
+        // A
+        //       E       J
+        //   B         H
 
-        const A = [100, 222]
-        const B = [178, 291]
-        const C = [178, 153]
-        const D = [178, 83]
-        const E = [250, 222]
-        const F = [288, 83]
-        const G = [342, 163]
-        const H = [398, 280]
-        const I = [420, 250]
-        const J = [398, 83]
+        const A = [1, 3]
+        const B = [2, 5]
+        const C = [2, 2]
+        const D = [2, 1]
+        const E = [4, 4]
+        const F = [5, 1]
+        const G = [6, 2]
+        const H = [7, 5]
+        const I = [7, 1]
+        const J = [8, 4]
 
         const pts = [B, C, A, H, E, J, I, F, D, G]
 
@@ -31,11 +32,11 @@ describe('Delaunay', () => {
         expect(adj[C].ToArray()).toEqual([A, B, E, F, D])
         expect(adj[D].ToArray()).toEqual([A, C, F])
         expect(adj[E].ToArray()).toEqual([C, B, H, G, F])
-        expect(adj[F].ToArray()).toEqual([D, C, E, G, J])
-        expect(adj[G].ToArray()).toEqual([F, E, H, I, J])
-        expect(adj[H].ToArray()).toEqual([I, G, E, B])
-        expect(adj[I].ToArray()).toEqual([J, G, H])
-        expect(adj[J].ToArray()).toEqual([F, G, I])
+        expect(adj[F].ToArray()).toEqual([D, C, E, G, I])
+        expect(adj[G].ToArray()).toEqual([F, E, H, J, I])
+        expect(adj[H].ToArray()).toEqual([J, G, E, B])
+        expect(adj[I].ToArray()).toEqual([F, G, J])
+        expect(adj[J].ToArray()).toEqual([I, G, H])
 
         const edges = UniqueEdges(adj)
         expect(edges.length).toEqual(20)
--- __tests__/MergeTest.js
+++ __tests__/MergeTest.js
@@ -5,21 +5,22 @@ import Merge from '../src/Merge'
 describe('Merge', () => {
 
     it('merges', () => {
-        //   D   F    J
-        //   C     G
-        // A   E      I
-        //   B        H
+        //   D     F   I
+        //   C       G
+        // A
+        //       E       J
+        //   B         H
 
-        const A = [10, 22]
-        const B = [18, 29]
-        const C = [18, 15]
-        const D = [18, 8]
-        const E = [25, 22]
-        const F = [29, 8]
-        const G = [34, 16]
-        const H = [40, 28]
-        const I = [42, 25]
-        const J = [40, 8]
+        const A = [1, 3]
+        const B = [2, 5]
+        const C = [2, 2]
+        const D = [2, 1]
+        const E = [4, 4]
+        const F = [5, 1]
+        const G = [6, 2]
+        const H = [7, 5]
+        const I = [7, 1]
+        const J = [8, 4]
 
         const adj = {}
 
@@ -37,15 +38,15 @@ describe('Merge', () => {
 
         Triangulate3(adj, F, G, H)
         Triangulate2(adj, I, J)
-        const [L2, R2] = LowerCommonTangent(adj, H, I)
+        const [L2, R2] = LowerCommonTangent(adj, H, J)
 
         Merge(adj, L2, R2)
 
-        expect(adj[F].ToArray()).toEqual([H, G, J])
-        expect(adj[G].ToArray()).toEqual([F, H, I, J])
-        expect(adj[H].ToArray()).toEqual([I, G, F])
-        expect(adj[I].ToArray()).toEqual([J, G, H])
-        expect(adj[J].ToArray()).toEqual([F, G, I])
+        expect(adj[F].ToArray()).toEqual([H, G, I])
+        expect(adj[G].ToArray()).toEqual([F, H, J, I])
+        expect(adj[H].ToArray()).toEqual([J, G, F])
+        expect(adj[I].ToArray()).toEqual([F, G, J])
+        expect(adj[J].ToArray()).toEqual([I, G, H])
 
         const [L3, R3] = LowerCommonTangent(adj, E, F)
         expect([L3, R3]).toEqual([B, H])
@@ -56,10 +57,10 @@ describe('Merge', () => {
         expect(adj[C].ToArray()).toEqual([A, B, E, F, D])
         expect(adj[D].ToArray()).toEqual([A, C, F])
         expect(adj[E].ToArray()).toEqual([C, B, H, G, F])
-        expect(adj[F].ToArray()).toEqual([D, C, E, G, J])
-        expect(adj[G].ToArray()).toEqual([F, E, H, I, J])
-        expect(adj[H].ToArray()).toEqual([I, G, E, B])
-        expect(adj[I].ToArray()).toEqual([J, G, H])
-        expect(adj[J].ToArray()).toEqual([F, G, I])
+        expect(adj[F].ToArray()).toEqual([D, C, E, G, I])
+        expect(adj[G].ToArray()).toEqual([F, E, H, J, I])
+        expect(adj[H].ToArray()).toEqual([J, G, E, B])
+        expect(adj[J].ToArray()).toEqual([I, G, H])
+        expect(adj[I].ToArray()).toEqual([F, G, J])
     })
 })
